92572aa0075ed5856bf8c08398bcb16dbe617208
Mikhail Yutman <myutman@gmail.com>
2019-03-08 15:05:46 +0300

    working moves

--- src/main/kotlin/ru/hse/supertux3/ui/CLI.kt
+++ src/main/kotlin/ru/hse/supertux3/ui/CLI.kt
@@ -3,91 +3,12 @@ package ru.hse.supertux3.ui
 import com.github.ajalt.mordant.TermColors
 import org.jline.terminal.Attributes
 import org.jline.terminal.TerminalBuilder
+import ru.hse.supertux3.levels.LevelGenerator
 
-val height = 10
-val width = 10
 
-val field = Array(height) {
-    CharArray(width) {
-        '.'
-    }
-}
+fun main() {
+    val visual = TermColors(TermColors.Level.TRUECOLOR)
 
-var i = 0
-var j = 0
-
-fun TermColors.init() {
-    field[0][0] = '@'
-    print("\u001Bc")
-    for (line in field) {
-        println(rgb("ff0000")(line.joinToString("")))
-    }
-    print(cursorUp(height))
-    print(strikethrough)
-}
-
-fun check(i: Int, j: Int): Boolean {
-    return i >= 0 && i < height && j >= 0 && j < width
-}
-
-fun TermColors.moveUp() {
-    if (check(i - 1, j)) {
-        field[i][j] = '.'
-        i--
-        field[i][j] = '@'
-
-        print('.')
-        print(cursorLeft(1))
-        print(cursorUp(1))
-        print('@')
-        print(cursorLeft(1))
-    }
-}
-
-fun TermColors.moveDown() {
-    if (check(i + 1, j)) {
-        field[i][j] = '.'
-        i++
-        field[i][j] = '@'
-
-        print('.')
-        print(cursorLeft(1))
-        print(cursorDown(1))
-        print('@')
-        print(cursorLeft(1))
-    }
-}
-
-fun TermColors.moveLeft() {
-    if (check(i, j - 1)) {
-        field[i][j] = '.'
-        j--
-        field[i][j] = '@'
-
-        print('.')
-        print(cursorLeft(1))
-        print(cursorLeft(1))
-        print('@')
-        print(cursorLeft(1))
-
-    }
-}
-
-fun TermColors.moveRight() {
-    if (check(i, j + 1)) {
-        field[i][j] = '.'
-        j++
-        field[i][j] = '@'
-
-        print('.')
-        print(cursorLeft(1))
-        print(cursorRight(1))
-        print(red("@"))
-        print(cursorLeft(1))
-    }
-}
-
-fun main() = TermColors(TermColors.Level.TRUECOLOR).run {
     val attributes = Attributes()
     //attributes.setLocalFlag(Attributes.LocalFlag.ECHOE, true)
     attributes.setLocalFlag(Attributes.LocalFlag.ECHO, false)
@@ -99,27 +20,33 @@ fun main() = TermColors(TermColors.Level.TRUECOLOR).run {
     terminal.enterRawMode()
     val reader = terminal.reader()
 
-    init()
+    val level = LevelGenerator.generate(4, 30, 40)
+    println("Generated")
+    val view = View(level, visual)
 
-    while (true) {
+    visual.run {
 
-        val buffer = CharArray(4)
-        var read = 0
-        while (read == 0) {
-            read = reader.read(buffer)
+        while (true) {
+
+            val buffer = CharArray(4)
+            var read = 0
+            while (read == 0) {
+                read = reader.read(buffer)
+            }
+
+            var quit = false
+            when (buffer[0]) {
+                'q' -> quit = true
+                'w' -> view.moveUp()
+                'a' -> view.moveLeft()
+                'd' -> view.moveRight()
+                's' -> view.moveDown()
+            }
+
+            if (quit) {
+                break
+            }
         }
-
-        var quit = false
-        when (buffer[0]) {
-            'q' -> quit = true
-            'w' -> moveUp()
-            'a' -> moveLeft()
-            'd' -> moveRight()
-            's' -> moveDown()
-        }
-
-        if (quit) {
-            break
-        }
+        print("\u001Bc")
     }
 }
--- src/main/kotlin/ru/hse/supertux3/ui/View.kt
+++ src/main/kotlin/ru/hse/supertux3/ui/View.kt
@@ -1,2 +1,133 @@
 package ru.hse.supertux3.ui
 
+import com.github.ajalt.mordant.TermColors
+import ru.hse.supertux3.levels.Coordinates
+import ru.hse.supertux3.levels.Direction
+import ru.hse.supertux3.levels.Level
+import ru.hse.supertux3.levels.Wall
+
+class View(val level: Level, val visual: TermColors) {
+    private var position: Coordinates = level.randomCell().coordinates
+
+    init {
+        print("\u001Bc")
+
+        for (i in 0 until level.height) {
+            for (j in 0 until level.width) {
+                val symb = level.getCell(i, j, position.h).toString()
+                visual.run {
+                    print(rgb("#ffffff")(symb))
+                }
+            }
+            visual.run {
+                println()
+            }
+        }
+
+        val up = level.height - position.i
+        val right = position.j
+        visual.run {
+            print(cursorUp(up))
+            print(cursorRight(right))
+            print(red("@"))
+            print(cursorLeft(1))
+        }
+        printPos()
+    }
+
+
+
+    fun check(position: Coordinates): Boolean {
+        return level.canGo(position, Direction.UP, 0)
+                && level.getCell(position) !is Wall
+    }
+
+    fun moveUp() {
+        if (check(position.copy(i = position.i - 1))) {
+            val symb = level.getCell(position).toString()
+            position = position.copy(i = position.i - 1)
+
+            visual.run {
+                print(symb)
+                print(cursorLeft(1))
+                print(cursorUp(1))
+                print(red("@"))
+                print(cursorLeft(1))
+            }
+        }
+        printPos()
+    }
+
+    fun moveDown() {
+        if (check(position.copy(i = position.i + 1))) {
+            val symb = level.getCell(position).toString()
+            position = position.copy(i = position.i + 1)
+
+            visual.run {
+                print(symb)
+                print(cursorLeft(1))
+                print(cursorDown(1))
+                print(red("@"))
+                print(cursorLeft(1))
+            }
+        }
+        printPos()
+    }
+
+    fun moveLeft() {
+        if (check(position.copy(j = position.j - 1))) {
+            val symb = level.getCell(position).toString()
+            position = position.copy(j = position.j - 1)
+
+
+            visual.run {
+                print(symb)
+                print(cursorLeft(1))
+                print(cursorLeft(1))
+                print(red("@"))
+                print(cursorLeft(1))
+            }
+
+        }
+        printPos()
+    }
+
+    fun moveRight() {
+        if (check(position.copy(j = position.j + 1))) {
+            val symb = level.getCell(position).toString()
+            position = position.copy(j = position.j + 1)
+
+
+            visual.run {
+                print(symb)
+                print(cursorLeft(1))
+                print(cursorRight(1))
+                print(red("@"))
+                print(cursorLeft(1))
+            }
+        }
+        printPos()
+    }
+
+    fun printPos() {
+
+        val up = level.height - position.i
+        val right = position.j
+
+        val str = buildString {
+            append("pos(i,j) = ", position.i, ", ", position.j, " height = ", level.height, " width = ", level.width, "      ")
+        }
+
+        visual.run {
+            print(cursorLeft(right))
+            print(cursorDown(up))
+
+
+            print(str)
+            print(cursorLeft(str.length))
+
+            print(cursorUp(up))
+            print(cursorRight(right))
+        }
+    }
+}
