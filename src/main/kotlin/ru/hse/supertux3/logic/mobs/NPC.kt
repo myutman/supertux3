a6fd8e69f58d707959bc6c3e7e11b4f975f75004
subject-name-here <jura.paradov@gmail.com>
2019-04-16 12:27:16 +0300

    added decorator

--- src/main/kotlin/ru/hse/supertux3/logic/mobs/NPC.kt
+++ src/main/kotlin/ru/hse/supertux3/logic/mobs/NPC.kt
@@ -6,8 +6,7 @@ import ru.hse.supertux3.logic.MoveResult
 import ru.hse.supertux3.logic.mobs.strategy.MoveStrategy
 
 /**
- * Interface for non-playable characters. They are mobs, but also they must move by themselves.
- * Also they can fight for cell, if someone stands there.
+ * Interface for non-playable characters. They must move by themselves.
  */
 abstract class NPC(cell: Cell, id: String) : Mob(cell, id) {
     /**
---
+++ src/main/kotlin/ru/hse/supertux3/logic/mobs/decorators/MobDecorator.kt
@@ -0,0 +1,30 @@
+package ru.hse.supertux3.logic.mobs.decorators
+
+import ru.hse.supertux3.logic.mobs.Mob
+
+class MobDecorator(val mob: Mob) : Mob(mob.cell, mob.id) {
+
+    init {
+
+    }
+
+    override var hp: Int
+        get() = mob.hp
+        set(value) { mob.hp = value }
+
+    override var resistChance: Int
+        get() = mob.resistChance
+        set(value) { mob.resistChance = value }
+
+    override var armor: Int
+        get() = mob.armor
+        set(value) { mob.armor = value }
+
+    override var damage: Int
+        get() = mob.damage
+        set(value) { mob.damage = value }
+
+    override var criticalChance: Int
+        get() = mob.criticalChance
+        set(value) { mob.criticalChance = value }
+}
