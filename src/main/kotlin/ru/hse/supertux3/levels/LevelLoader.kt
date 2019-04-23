e745a8a26f306c1ae466477725f02c7efa136a55
iisuslik <al.s.fedotov1998@yandex.ru>
2019-04-23 22:44:22 +0300

    load state

--- src/main/kotlin/ru/hse/supertux3/levels/LevelLoader.kt
+++ src/main/kotlin/ru/hse/supertux3/levels/LevelLoader.kt
@@ -1,8 +1,15 @@
 package ru.hse.supertux3.levels
 
+import ru.hse.supertux3.logic.GameState
+
 class LevelLoader {
     fun loadLevel(fileName: String): Level {
         return Level.load(fileName)
+    }
+
+    fun loadGameState(fileName: String): GameState {
+        val level = Level.load(fileName)
+        return GameState(level, level.player!!) //TODO
     }
 
     fun generateLevel(): Level {
