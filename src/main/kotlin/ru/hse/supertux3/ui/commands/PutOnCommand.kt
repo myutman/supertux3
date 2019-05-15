5df391010bbf862298ad371da6fc79bd5ff2dfd2
subject-name-here <jura.paradov@gmail.com>
2019-05-15 18:46:01 +0300

    moved putOff and putOn logic from command to model

--- src/main/kotlin/ru/hse/supertux3/ui/commands/PutOnCommand.kt
+++ src/main/kotlin/ru/hse/supertux3/ui/commands/PutOnCommand.kt
@@ -6,43 +6,7 @@ import ru.hse.supertux3.ui.readChar
 import java.lang.RuntimeException
 
 class PutOnCommand(val model: Model): Command {
-    private fun message(str: String) {
-        model.view.printMessage("$str${System.lineSeparator()}Press ESC to continue")
-        while (true) {
-            if (readChar().toInt() == 27) break
-        }
-        model.view.redraw()
-    }
-
     override fun execute() {
-        model.view.printMessage("What do you want to put on")
-        val slot = readChar()
-        val equipped = model.state.player.inventory.equipped
-        val unequipped = model.state.player.inventory.unequipped
-        try {
-            val info = model.state.player.inventory.getItemInfoBySlot(slot)
-            if (info.isEquipped) {
-                message("Item is already equipped")
-                return
-            }
-            val item = unequipped[info.index]
-            if (item !is Wearable) {
-                message("Item is not wearable")
-                return
-            }
-            if (equipped.containsKey(item.type)) {
-                message("${item.type} is already equipped")
-                return
-            }
-            item.putOn(model.state.player)
-            unequipped.removeAt(info.index)
-            equipped.put(item.type, item)
-            model.view.redraw()
-        } catch (e: RuntimeException) {
-            message(e.message!!)
-            return
-        }
-
-        model.afterAction()
+        model.putOn()
     }
 }
--- src/main/kotlin/ru/hse/supertux3/ui/commands/TakeOffCommand.kt
+++ src/main/kotlin/ru/hse/supertux3/ui/commands/TakeOffCommand.kt
@@ -4,36 +4,7 @@ import ru.hse.supertux3.logic.Model
 import ru.hse.supertux3.ui.readChar
 
 class TakeOffCommand(val model: Model) : Command {
-    private fun message(str: String) {
-        model.view.printMessage("$str${System.lineSeparator()}Press ESC to continue")
-        while (true) {
-            if (readChar().toInt() == 27) break
-        }
-        model.view.redraw()
-    }
-
     override fun execute() {
-        model.view.printMessage("What do you want to put off")
-        val slot = readChar()
-        val equipped = model.state.player.inventory.equipped
-        val unequipped = model.state.player.inventory.unequipped
-        try {
-            val info = model.state.player.inventory.getItemInfoBySlot(slot)
-            if (!info.isEquipped) {
-                message("Item is not equipped")
-                return
-            }
-            val entry = equipped.toList()[info.index]
-            entry.second.takeOff(model.state.player)
-            equipped.remove(entry.first)
-            unequipped.add(model.state.player.inventory.inventoryCur, entry.second)
-            model.view.redraw()
-        } catch (e: RuntimeException) {
-            message(e.message!!)
-            return
-        }
-
-        model.afterAction()
+        model.putOff()
     }
-
 }
